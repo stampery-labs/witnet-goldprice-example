53323ea01c996fa2d2f6daef78a5f45707210c7a
Mario Cao <mariocao@gmail.com>
2020-03-30 18:17:14 +0200

    chore: add initial project setup
    Co-authored-by: Gorka Irazoqui <gorka.irazoki@gmail.com>

---
+++ flattened-config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  compilers: {
+    solc: {
+      version: "0.6.4",
+      settings: { optimizer: { enabled: true, runs: 200 } }
+    },
+  },
+  contracts_directory: "./contracts/flattened/",
+  networks: {
+    development: {
+      provider: require("ganache-cli").provider({ gasLimit: 100000000 }),
+      network_id: "*",
+    },
+    mainnet: {
+      network_id: 1,
+      host: "localhost",
+      port: 8541,
+    },
+    rinkeby: {
+      network_id: 4,
+      host: "localhost",
+      port: 8544,
+    },
+    ropsten: {
+      network_id: 3,
+      host: "localhost",
+      port: 8543,
+    },
+    goerli: {
+      network_id: 5,
+      host: "localhost",
+      port: 8545,
+    },
+  },
+}
